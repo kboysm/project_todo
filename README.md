c5d0d3266c6bf30567338a883ffb8df6a6c19e64
kboysm <lishal12@naver.com>
2021-01-06 20:54:49 +0900

    sideBar

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 ## 2021 - 01 -05
 figma를 이용한 design , figma-design 폴더 대략적인 design 및 figma 경험
+
+## 2021 - 01 -06
+figma로 디자인한 화면 만들기 , 목표 : 공통으로 사용되는 component layout 만들기
---
+++ src/components/sideBar/SideBar.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="sideBar">
+        <button>
+            <img src="../../assets/Icon/face_unlock_24px.svg" alt="user">
+        </button>
+        <button>
+            <img src="../../assets/Icon/event_available_24px.svg" alt="user">
+        </button>
+        <button>
+            <img src="../../assets/Icon/folder_24px.svg" alt="user">
+        </button>
+        <button>
+            <img src="../../assets/Icon/Group.svg" alt="user">
+        </button>
+        <button>
+            <img src="../../assets/Icon/logout.svg" alt="user">
+        </button>
+        
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+
+    @Component
+    export default class SideBar extends Vue {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
