19efcb266cedc243a9add30ca60016448112b6d8
kboysm <lishal12@naver.com>
2021-01-15 00:24:06 +0900

    FilePage

--- src/components/RouteComponent/FilePage.vue
+++ src/components/RouteComponent/FilePage.vue
@@ -5,7 +5,17 @@
         <input type="text"
         id="searchBar"
         placeholder=" "
-        class="border-b-2 w-11/12 h-full inline-block focus:outline-none focus:ring focus:border-blue-300" />
+        class="border-b-2 w-11/12 h-full inline-block focus:outline-none" />
+        </div>
+        <div class="flex flex-row-reverse pr-12 pt-3">
+            <img class="inline-block" src="../../assets/Icon/plusBtn.svg" alt="">
+        </div>
+        <div class="border grid grid-cols-4 gap-4 pl-12 ml-12">
+            <div class="flex justify-center item-center w-64 h-40 bg-gray-200 rounded-lg"
+            v-for="( item , index ) in 12" :key="index"
+            >
+                <img src="../../assets/Icon/lock.svg" alt="">
+            </div>
         </div>
     </div>
 </template>
